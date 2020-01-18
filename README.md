a829308e3a0290166c11a21e3113d1597fece278
Haikel Fazzani <haikel.fazzani@hotmail.com>
2020-01-18 15:22:44 +0100

    feat: run code

--- README.md
+++ README.md
@@ -38,6 +38,48 @@ example : https://open-rest.herokuapp.com/api/github/haikelfazzani
 ...etc]
 ```
 
+### Run code:
+- POST /api/runcodev2  
+# ['C', 'CPP', 'CPP11', 'CPP14', 'CLOJURE', 'CSHARP', 'GO', 'HASKELL', 
+# 'JAVA', 'JAVA8', 'JAVASCRIPT', 'JAVASCRIPT_NODE', 'OBJECTIVEC', 'PASCAL', 
+# 'PERL', 'PHP', 'PYTHON', 'PYTHON3', 'R', 'RUBY', 'RUST', 'SCALA', 'SWIFT', 'SWIFT_4_1']  
+ 
+Example : http://localhost:5000/api/runcodev2/python3/print(5)
+```json
+{
+  "async": 0,
+  "memory_limit": 262144,
+  "memory_used": "64",
+  "output": "5\n",
+  "output_html": "5<br>",
+  "request_NOT_OK_reason": "",
+  "request_OK": "True",
+  "signal": "OTHER",
+  "status": "AC",
+  "status_detail": "NA",
+  "stderr": "",
+  "time_limit": 5,
+  "time_used": "0.105635"
+}
+```
+- POST /api/runcode  
+languages = ['C', 'Cpp', 'Cpp14', 'Java', 'Python', 'Python3', 'Scala', 'Php', 'Perl', 'Csharp']
+
+```json
+{
+  "cmpError": "",
+  "compResult": "S",
+  "hash": "815b333dde20afc694d4757ceed36e4e_Tester_U16",
+  "id": "9CNgKChGku",
+  "lxcOutput": "",
+  "memory": "0.125",
+  "output": "5\n",
+  "rntError": "",
+  "time": "0.02",
+  "valid": "1"
+}
+```
+
 ### dev commands
 - pipenv shell
 - flask run
--- app.py
+++ app.py
@@ -6,7 +6,32 @@ langs = ['java', 'javascript','typescript','css','html',
 'sql','plsql','plpgsql',
 'php','python', 'go', 'rust', 'c', 'c++', 'csharp', 'dart', 'hack', 'shell']
 
+languages = ['C', 'Cpp', 'Cpp14', 'Java', 'Python', 'Python3', 'Scala', 'Php', 'Perl', 'Csharp']
+
 app = Flask(__name__)
+
+# ['C', 'CPP', 'CPP11', 'CPP14', 'CLOJURE', 'CSHARP', 'GO', 'HASKELL', 
+# 'JAVA', 'JAVA8', 'JAVASCRIPT', 'JAVASCRIPT_NODE', 'OBJECTIVEC', 'PASCAL', 
+# 'PERL', 'PHP', 'PYTHON', 'PYTHON3', 'R', 'RUBY', 'RUST', 'SCALA', 'SWIFT', 'SWIFT_4_1']
+@app.route('/api/runcodev2/<lang>/<code>')
+def run_code(lang,code):
+	# 73a1fc29f9a43c67a8bc996777ebb248d9b8b9cd
+	data = {
+		'client_secret': '69c68a6905f73de654c15c05744a5e93147c1132',
+		'async': 0,
+		'source': code,
+		'lang': lang.upper(),
+		'time_limit': 5,
+		'memory_limit': 262144,
+	}
+	r = requests.post(u'https://api.hackerearth.com/v3/code/run/', data=data)
+	return r.json()["run_status"]
+
+@app.route('/api/runcode/<lang>/<code>')
+def gfg_compile(lang,code):
+    data = {'lang': lang,'code': code,'input': None,'save': False}
+    r = requests.post("https://ide.geeksforgeeks.org/main.php", data=data)
+    return r.json()
 
 @app.route('/api/github/<username>')
 def index(username):
