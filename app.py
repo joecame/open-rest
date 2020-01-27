8109ac02314ce61d9aab5f06020856d8bc005364
Joe Cameron <43419386+joecame@users.noreply.github.com>
2020-01-27 13:58:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from routes.home import home
 from routes.github import github
 from routes.runcode import runcode
 from routes.atom import atom
@@ -8,6 +9,7 @@ app = Flask(__name__)
 app.debug = True
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+app.register_blueprint(home, url_prefix='/')
 app.register_blueprint(github, url_prefix='/api/github')
 app.register_blueprint(atom, url_prefix='/')
 app.register_blueprint(runcode, url_prefix='/')
